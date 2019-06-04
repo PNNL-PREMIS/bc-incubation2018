034954441cfe74aeea08aa93ab7d03545eb2c342
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-06-04 01:43:00 -0400

    Working on reading the valvemap
    But see #1 and #2

--- 3-summarize (qc).r
+++ 3-summarize (qc).r
@@ -21,7 +21,7 @@ openlog(file.path(outputdir(), paste0(SCRIPTNAME, ".log.txt")), sink = TRUE) # o
 printlog("Welcome to", SCRIPTNAME)
 
 printlog("Reading in raw data...")
-read_csv(RAWDATA_FILE, col_types = "ccccidddd") %>%
+read_csv(RAWDATA_FILE, col_types = "cccidddd") %>%
   # Convert date/time to POSIXct
   mutate(DATETIME = ymd_hms(paste(DATE, TIME))) %>%
   select(-DATE, -TIME) %>%
@@ -49,10 +49,10 @@ rawdata %>%
   mutate(elapsed_seconds = as.double(difftime(DATETIME, min(DATETIME), units = "secs"))) ->
   rawdata_samples
 
-printlog("Removing ambient samples...")
-AMBIENT_VALVE <- 16
+printlog("Removing ambient and bad (valve 2) samples...")
+AMBIENT_VALVE <- c(0, 2, 16)
 rawdata_samples %>%
-  filter(MPVPosition != AMBIENT_VALVE) %>% 
+  filter(!MPVPosition %in% AMBIENT_VALVE) %>% 
   group_by(samplenum) %>%
   #  filter(max(elapsed_seconds) <= MAX_MEASUREMENT_TIME) %>%
   print_dims("rawdata_samples") ->
@@ -61,9 +61,46 @@ rawdata_samples %>%
 printlog("Visualizing...")
 p <- ggplot(rawdata_samples, aes(x = elapsed_seconds, color = yday(DATETIME), group = samplenum)) +
   facet_wrap(~MPVPosition, scales = "free_y") + 
-  ggtitle("Concentration by date and valve")
-print(p + geom_line(aes(y = CO2_dry)) + xlim(c(0, 60)))
+  ggtitle("Concentration by date and day of year") +
+  xlab("Elapsed time (s)") + scale_color_continuous("Day")
+print(p + geom_line(aes(y = CO2_dry)) + xlim(c(0, 60))) + ylab("CO2 (ppm)")
 save_plot("co2_by_valve", ptype = ".png")
-print(p + geom_line(aes(y = CH4_dry)) + xlim(c(0, 60)))
+print(p + geom_line(aes(y = CH4_dry)) + xlim(c(0, 60))) + ylab("CH4 (ppb)")
 save_plot("ch4_by_valve", ptype = ".png")
 
+clean_aditi_time_column <- function(x) {
+  
+}
+
+# The 'valvemap' data maps Picarro valve numbers to sample IDs
+printlog(SEPARATOR)
+printlog("Reading valve and core mapping data...")
+read_csv(VALVEMAP_FILE) %>% 
+  mutate(rownum = row_number()) %>% 
+  filter(!is.na(Core)) %>%
+  mutate(Time = gsub("\\.", ":", Time)) %>% 
+  separate(Time, into = c("Start_time", "Stop_time"), sep = "-", remove = FALSE) %>% 
+  # ADITI SENGUPTA YOU ARE KILLING ME HERE
+  # Completely inconsistent recording of times. Arrrgh!
+  # Periods vs. colons, AM/PM versus nothing, ".-", ...
+  mutate(Start_time = gsub("(A|P)M", "", Start_time)) %>% 
+  separate(Start_time, into = c("hr", "min"), sep =":", remove = FALSE, convert = TRUE) %>% 
+  mutate(Start_time = paste(Start_time, if_else(hr < 7, "PM", "AM"))) %>% 
+  
+  mutate(Stop_time = gsub("(A|P)M", "", Stop_time)) %>% 
+  separate(Stop_time, into = c("hr", "min"), sep =":", remove = FALSE, convert = TRUE) %>% 
+  mutate(Stop_time = paste(Stop_time, if_else(hr < 7, "PM", "AM"))) %>% 
+  
+  mutate(Picarro_start = mdy_hm(paste(Date, Start_time), tz = "America/Los_Angeles"),
+         Picarro_stop = mdy_hm(paste(Date, Stop_time), tz = "America/Los_Angeles")) %>% 
+  
+  # Oh ffs what's going on with the `Core` column here?
+  separate(Core, into = c("Core", "something_else"), sep = "-") %>% 
+  select(Core, Picarro_start, Picarro_stop, Headspace, Valve, rownum) %>% 
+  arrange(Picarro_start) ->
+  valvemap
+
+# valvemap diagnostic
+ggplot(valvemap, aes(Picarro_start, Valve, color = Core)) + 
+  geom_point()
+save_plot("valvemap", height = 4, width = 8)
